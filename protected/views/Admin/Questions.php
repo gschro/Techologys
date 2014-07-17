ef712da58e1c89600509326e18055ce0c062c9bb
gschro <gtschroath@gmail.com>
2014-07-17 00:03:00 -0500

    trying something out

--- protected/views/Admin/Questions.php
+++ protected/views/Admin/Questions.php
@@ -73,7 +73,7 @@ select{
                     <h4>Add a Category Pair</h4>
                         <label>Categories</label>
                       <!--  <table style='width:370px;'>
-                            <tr>PLEASEHELP!!!
+                            <tr>PLEASEHELPWITHHOOKS!!!
                                 <td style='width:160px;'>
                                     <b>Not Available</b><br/>
                                    <select multiple="multiple" size="15" id='lstBox1' class='listBox'>
